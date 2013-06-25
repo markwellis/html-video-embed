aa0cb8d476358a4972907343c932992b319ea156
Mark Ellis <m@rkellis.com>
2013-06-25 11:19:48 +0100

    version bump

--- lib/HTML/Video/Embed.pm
+++ lib/HTML/Video/Embed.pm
@@ -8,7 +8,7 @@ use URI::Escape::XS;
 use Data::Validate::URI qw/is_web_uri/;
 use Module::Find;
 
-our $VERSION = '0.013000';
+our $VERSION = '0.014000';
 $VERSION = eval $VERSION;
 
 has 'class' => (
